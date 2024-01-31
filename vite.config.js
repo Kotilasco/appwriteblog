36831bfb4e5c358da26a9acb8172204cc4517cff
kuly87 <69579254+kuly87@users.noreply.github.com>
2024-01-31 10:38:12 +0200

    vite conf

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         "@reduxjs/toolkit",
         "html-react-parser",
         "react-hook-form",
+        "@tinymce/tinymce-react",
       ],
       output: {
         globals: {
